1879afdcf72f7854a4e6bf57282312a5f76e22ae
Dzmitry Izaitka <dem214overlord@gmail.com>
2021-01-13 22:47:16 +0300

    seed complete

--- my_little_office/apps/staff/management/commands/seed.py
+++ my_little_office/apps/staff/management/commands/seed.py
@@ -2,6 +2,7 @@ from random import choice, randint
 from decimal import Decimal
 
 from django.core.management.base import BaseCommand
+from django.db.transaction import atomic
 
 from ...models import Employee, Position
 
@@ -25,14 +26,30 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        def gen_employee(positions, parent=''):
+        def gen_employee(positions, parents):
             names = fake.name().split(' ')
+            parent = choice(parents)
             return Employee(
                 second_name=names[-3],
                 first_name=names[-2],
                 patronim=names[-1],
-                salary=Decimal(randint(10000, 100000) / 100),
+                position=choice(positions),
+                salary=Decimal(randint(10000, 50000) / 100),
+                parent=parent,
+                lft=1,
+                rght=1,
+                tree_id=parent.tree_id,
+                level=parent.level+1,
             )
+
+        def create_level_staff(amount, parents, positions):
+            print(f'populating {amount} level 1 staff.')
+            staff_presave = []
+            for i in range(amount):
+                staff_presave.append(gen_employee(positions, parents))
+            staff = Employee.objects.bulk_create(staff_presave)
+            print(f'populated: {", ".join(e.full_name for e in staff)}.')
+            return staff
 
         from faker import Faker
         fake = Faker('ru_RU')
@@ -44,9 +61,23 @@ class Command(BaseCommand):
         print(f'populated: {", ".join(p.name for p in positions)}')
 
         print(f'populating {options["staff_l0"]} level 0 staff')
-        level0 = Employee.objects.bulk_create((
-            gen_employee(positions) for i in range(options['staff_l0'])
-        ))
+        level0 = list()
+        for i in range(options['staff_l0']):
+            names = fake.name().split(' ')
+            level0.append(Employee.objects.create(
+                second_name=names[-3],
+                first_name=names[-2],
+                patronim=names[-1],
+                position=choice(positions),
+                salary=Decimal(randint(10000, 100000) / 100),
+            ))
         print(f'populated: {", ".join(e.full_name for e in level0)}')
+
+        level1 = create_level_staff(options['staff_l1'], level0, positions)
+        level2 = create_level_staff(options['staff_l2'], level1, positions)
+        level3 = create_level_staff(options['staff_l3'], level2, positions)
+        level4 = create_level_staff(options['staff_l4'], level3, positions)
+
+        Employee.objects.rebuild()
 
         print('done')
